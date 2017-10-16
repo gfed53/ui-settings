927f66ec7bc3f24ce6d9a0b044a715c881719737
Gregory Federico <gfederico7153@gmail.com>
2017-10-16 17:03:03 -0400

    Added handlers for mouseleave, knob containers, text to work with, other tweaks

--- js/script.js
+++ js/script.js
@@ -3,14 +3,25 @@
 $(function(){
 	let int001;
 
-	$('.knob').mousedown(function(){
-		$(this).addClass('activated');
+	$('.knob-container').mousedown('.knob', function(e){
+		e.preventDefault();
+		$(this).children('.knob').addClass('activated');
+		let count = 0;
 		
 		int001 = setInterval(() => {
-			console.log(this);
-		}, 100);
-	}).mouseleave(function(){
-		$(this).removeClass('activated');
+			// console.log(this);
+			console.log('int001',int001);
+			count++;
+			console.log('count',count);
+		}, 1000);
+		// console.log('int001',int001);
+	}).mouseup(function(e){
+		e.preventDefault();
+		$(this).children('.knob').removeClass('activated');
+		clearInterval(int001);
+	}).mouseleave(function(e){
+		e.preventDefault();
+		$(this).children('.knob').removeClass('activated');
 		clearInterval(int001);
 	});
 });
